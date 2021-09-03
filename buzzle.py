5dad1f5adaa962902be0638beb5eab4079566ac6
BenjaminMassey <benjamin.w.massey@gmail.com>
2021-09-03 03:31:52 -0700

    Generally working barebones-wise, but issues with closing out, and not full implementation i.e. clearing queue and nice looking

--- buzzle.py
+++ buzzle.py
@@ -1,14 +1,22 @@
 from flask import Flask, render_template, request
-import random
+from tkinter import *
+import threading
+import time
+
 app = Flask(__name__)
+
+players = []
 
 @app.route('/', methods = ['GET', 'POST'])
 def index():
+	global players
 	try:
 		result = request.form.get('name')
 	except:
 		result = None
 	print("Here is the result:", result)
+	if result is not None and result not in players:
+		players.append(result)
 	page = None
 	try:
 		page = render_template("index.html")
@@ -17,5 +25,34 @@ def index():
 		page = "Unknown Error"
 	return page
 
-if __name__ == '__main__':
-    app.run(host='127.0.0.1', port=8000)
+def SiteLoop():
+	if __name__ == '__main__':
+		app.run(host='127.0.0.1', port=8000)
+
+window = Tk()
+window.title("Buzzle Bee")
+window.geometry('300x600')
+
+def UpdaterLoop():
+	global window, players
+	
+	text = StringVar()
+	text.set("")
+	label = Label(window, textvariable=text)
+	label.config(font=("Courier", 24))
+	label.pack()
+	
+	while True:
+		queue = ""
+		for player in players:
+			queue += player + "\n"
+		text.set(queue)
+		time.sleep(0.2)
+
+site = threading.Thread(target=SiteLoop)
+site.start()
+
+updater = threading.Thread(target=UpdaterLoop)
+updater.start()
+
+window.mainloop()
